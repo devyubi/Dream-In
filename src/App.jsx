188516d6177d07f92e4b3a479edc7fabcc517ced
dev.clarova <dev.clarova@gmail.com>
2025-07-29 14:52:04 +0900

    [docs] README 수정 및 DreamWritePage 진행 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import DreamWritePage from "./pages/DreamWritePage";
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
         <Route path="/login" element={<h1>로그인</h1>}></Route>
         <Route path="/signup" element={<h1>회원가입</h1>}></Route>
         <Route path="/profile" element={<h1>프로필</h1>}></Route>
-        <Route path="/dreamwrite" element={<h1>꿈 작성</h1>}></Route>
+        <Route
+          path="/dreamwrite"
+          element={<DreamWritePage></DreamWritePage>}
+        ></Route>
         <Route path="/emotiondiary" element={<h1>감정일기</h1>}></Route>
         <Route path="/sleeprecord" element={<h1>수면기록</h1>}></Route>
         <Route path="/aidreamsresult" element={<h1>꿈 해몽</h1>}></Route>
--- src/pages/DreamWritePage.jsx
+++ src/pages/DreamWritePage.jsx
@@ -1,7 +1,87 @@
+import styled from "@emotion/styled";
 import React from "react";
+import Header from "../components/Header";
+
+// 전역(window) 자리
+const Container = styled.div`
+  position: relative;
+  max-width: 1280px;
+  margin: auto;
+  padding: 15px;
+  background-color: rgba(252, 243, 246, 0.43);
+`;
+const BackKey = styled.button`
+  padding: 7px;
+  border: none;
+  border-radius: 50%;
+  margin-bottom: 15px;
+  background-color: #fcf3fb;
+  box-shadow: 6px 6px 8px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  &&:hover {
+    background-color: #fad4e8;
+  }
+`;
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5px;
+  border-top: 1px solid #000;
+  padding-top: 10px;
+`;
+const Title = styled.h1`
+  margin: 0;
+  color: #493d78;
+`;
+const SubTitle = styled.h2`
+  margin: 0;
+  color: #493d78;
+`;
+const DreamTitle = styled.h2`
+  padding-top: 10px;
+  padding-left: 10px;
+  color: #493d78;
+`;
+const DreamTitleText = styled.input`
+  position: relative;
+  border: 1px solid #c8c8c8;
+  padding-top: 5px;
+  max-height: 50px;
+  border-radius: 16px;
+  background: linear-gradient(
+    to right,
+    rgba(230, 179, 247, 0.3),
+    rgba(211, 188, 232, 0.3),
+    rgba(194, 193, 238, 0.3)
+  );
+  width: 100%;
+  height: 50px;
+`;
+const TextNum = styled.span`
+  position: absolute;
+  right: 30px;
+  top: 200px;
+  color: #493d78;
+`;
 
 function DreamWritePage() {
-  return <div>DreamWritePage</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <BackKey>뒤로</BackKey>
+        <Top>
+          <Title>꿈 이야기 기록하기</Title>
+          <SubTitle>어젯밤 꾼 꿈을 아름다운 이야기로 남겨주세요.</SubTitle>
+        </Top>
+        <DreamTitle>꿈 제목</DreamTitle>
+        <DreamTitleText title="text"></DreamTitleText>
+        <TextNum>0/100</TextNum>
+      </Container>
+    </>
+  );
 }
 
 export default DreamWritePage;
